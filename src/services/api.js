3112df19c0d6b230d04f60d75f8c38170105bb68
Adiksha <challavenkataadiksha@gmail.com>
2026-07-30 15:24:39 +0530

    Update backend API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://movie-ticket-booking-backend-ex9s.onrender.com/api"
 });
 
 export default API;
